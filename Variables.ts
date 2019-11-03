8823bde34bbb9ca3ba3196afb7afedaae37ecb9a
AlaguSelvan <alaguselvan97@gmail.com>
2019-11-03 13:05:31 +0530

    first commit

---
+++ Variables.ts
@@ -0,0 +1,25 @@
+const apples: number = 4
+const myName: string = 'Dave'
+const mySkills: string[] = ['Gaming', 'Programming']
+
+const colors: string[] = ['red', 'black', 'orange'];
+const myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, false];
+
+// For Class
+
+class Car {}
+
+let car: Car = new Car();
+
+// Object Literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
+
+// Function
+const logNumbers = (i: number) => {
+  console.log(i)
+};
+
