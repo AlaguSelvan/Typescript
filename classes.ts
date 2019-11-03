f70914d648b9226d70306289786eb12d3973c42c
AlaguSelvan <alaguselvan97@gmail.com>
2019-11-03 20:57:00 +0530

    more comments

--- classes.ts
+++ classes.ts
@@ -11,6 +11,7 @@ class Vehicle {
 // inherits Vehicle class
 class Car1 extends Vehicle {
   constructor(public wheels: number, public color: string) {
+    // use super to initialize constructor in child class
     super('red');
   }
   private drive(): void {
